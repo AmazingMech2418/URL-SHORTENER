4a758d612135fea802003b31b0e18c4af5d724ad
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-03-16 16:59:19 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-var list = ["","#google","#scratch"];
-var urls = ["home","https://www.google.com","https://scratch.mit.edu"];
+var list = ["","#google","#scratch","#amazingmech2418"];
+var urls = ["home","https://www.google.com","https://scratch.mit.edu","https://amazinigmech2418.github.io"];
